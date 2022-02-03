25d0bb494e91e1ac9fc43c7742cd19e1b0aa604e
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-02 19:10:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("image1").addEventListener("click", displayAlert2);
+document.getElementById("image").addEventListener("click", displayAlert2);
 function displayAlert2(){
   alert("This is an image of the black pieces on a chessboard.");
   alert("Look below for some chess links!");
