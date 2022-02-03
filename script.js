e933f9ab8a3e10a05382b6a58af61675a69624dc
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-02 19:43:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 document.getElementById("image").addEventListener("click", displayAlert2);
-document.getElementsByClassName("links").addEventListener("click", displayAlert);
-function displayAlert(){
-  alert("This link is taking you out of my website and to another link. The link will open in a new tab. Click OK to proceed to the website.");
-}
 function displayAlert2(){
   alert("This is an image of the black pieces on a chessboard.");
   alert("Look below for some chess links!");
