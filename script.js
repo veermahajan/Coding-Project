addf613545cfe0386587fa9f619f6fa09b6776b8
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-03 15:27:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -35,15 +35,14 @@ for (let i = 0; i < websites.length; i++) {
   text += websites[i] + "<br>";
 }
 document.getElementById("websites").innerHTML = text;
+var text1 = "";
+var length = websites.length - 1;
+for (let x = 0; x < length; x++){
+  text1 += websites[x] + ", ";
+}
+text1 += "and" + websites[length] + ".";
+var textFinal = "Look above at the links for more information about the websites " + text1;
 
 function displayAlertSpecial(){
-  var websites1 = ["Chess.com", "2700chess.com", "My own website", "Kasparovchess.com"];
-  var text1 = "";
-  var length = websites1.length - 1;
-  for (let x = 0; x < length; x++){
-    text1 += websites1[i] + ", ";
-  }
-  text1 += "and" + websites1[length] + ".";
-  var textFinal = "Look above at the links for more information about the websites " + text1;
   alert(textFinal);
 }
