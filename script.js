f74e793ae0ec29c36d88b72474652b7ace72413c
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-02 20:02:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("image").addEventListener("click", displayAlert);
 document.getElementById("link1").addEventListener("click", displayAlertForLink1);
 document.getElementById("link2").addEventListener("click", displayAlertForLink2);
+document.getElementById("link3").addEventListener("click", displayAlertForLink3);
 
 function displayAlert(){
   alert("This is an image of the black pieces on a chessboard.");
@@ -13,4 +14,11 @@ function displayAlertForLink1(){
 function displayAlertForLink2(){
   alert("This is a description for my own chess website.");
   alert("There are videos and puzzles on it.");
+}
+function displayAlertForLink3(){
+  alert("This is a description for a chess website called 2700chess.com.");
+  alert("Here, you can view live games and tournament results.");
+  alert("You can also see live ratings, ratings from the past, all time records, and more. These can also be embedded into your own website.");
+  alert("In addition, there are also detailed pages about every player, and so many statistics to look at.");
+  alert("Be sure to check out 2700chess.com!");
 }
