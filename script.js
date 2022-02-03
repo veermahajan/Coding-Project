eba4548b24b965819cdbeb04478c3458c1ffd099
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-02 19:50:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function displayAlert(){
   alert("Look below for some chess links!");
 }
 function displayAlertForLink1(){
-  alert("This is an image of the black pieces on a chessboard.");
-  alert("Look below for some chess links!");
+  alert("This is a description for a chess website called Chess.com.");
+  alert("It is the world's largest and most popular chess website, and you can do many things on it!");
 }
 function displayAlertForLink2(){
-  alert("This is an image of the black pieces on a chessboard.");
-  alert("Look below for some chess links!");
+  alert("This is a description for my own chess website.");
+  alert("There are videos and puzzles on it.");
 }
