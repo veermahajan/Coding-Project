e68564aab6383bac62cd92f25ab581c7dd822a54
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-02 19:46:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-document.getElementById("image").addEventListener("click", displayAlert2);
-function displayAlert2(){
+document.getElementById("image").addEventListener("click", displayAlert);
+document.getElementById("link1").addEventListener("click", displayAlertForLink1);
+document.getElementById("link2").addEventListener("click", displayAlertForLink2);
+
+function displayAlert(){
+  alert("This is an image of the black pieces on a chessboard.");
+  alert("Look below for some chess links!");
+}
+function displayAlertForLink1(){
+  alert("This is an image of the black pieces on a chessboard.");
+  alert("Look below for some chess links!");
+}
+function displayAlertForLink2(){
   alert("This is an image of the black pieces on a chessboard.");
   alert("Look below for some chess links!");
 }
