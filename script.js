022a427bb227a3f2d1db980e049006df5164a865
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-02-03 18:37:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("link1").addEventListener("click", displayAlertForLink1)
 document.getElementById("link2").addEventListener("click", displayAlertForLink2);
 document.getElementById("link3").addEventListener("click", displayAlertForLink3);
 document.getElementById("link4").addEventListener("click", displayAlertForLink4);
+document.getElementById("link5").addEventListener("click", displayAlertForLink5);
 document.getElementById("websites").addEventListener("click", displayAlertSpecial);
 window.addEventListener("message",e=>{e['data']&&"8901807"===e['data']['id']&&document.getElementById(`${e['data']['id']}`)&&(document.getElementById(`${e['data']['id']}`).style.height=`${e['data']['frameHeight']+30}px`)});
 function displayAlert(){
@@ -15,6 +16,14 @@ function displayAlertForLink1(){
   alert("You can play against other players from around the world, and in tournaments. The game times vary from in the seconds to in the weeks! You can also play against computers of varying strengths.");
   alert("You can solve puzzles (do other puzzle related activities) and watch lessons.");
   alert("There is a lot more to do on Chess.com, so sign up for free!");
+}
+
+function displayAlertForLink5(){
+  alert("This is a description for the website of the United States Chess Federation.");
+  alert("You can sign up to get an official US Chess profile and ratings.");
+  alert("You can play online events (which are rated) for free on websites such as Chess.com.");
+  alert("You can also play national and local in-person events, which are also rated. You can compete for massive prizes, and for official titles.");
+  alert("Sign up for US Chess to play in online and in-person events, get ratings, and more!");
 }
 function displayAlertForLink4(){
   alert("This is a description for a chess website called Kasparovchess.com.");
@@ -34,12 +43,12 @@ function displayAlertForLink3(){
   alert("This is a description for a chess website called 2700chess.com.");
   alert("Here, you can view live games and tournament results.");
   alert("You can also see live ratings, ratings from the past, all time records, and more. These can also be embedded into your own website.");
-  alert("In addition, there are also detailed pages about every player, and so many statistics to look at.");
+  alert("In addition, there are also detailed pages about every player, and many statistics to look at.");
   alert("Be sure to check out 2700chess.com!");
 }
 
 var text = "";
-var websites = ["Chess.com", "2700chess.com", "Chess for Beginners and Intermediates - my own website", "Kasparovchess.com"];
+var websites = ["Chess.com", "2700chess.com", "Chess for Beginners and Intermediates - my own website", "Kasparovchess.com", "United States Chess Federation (US Chess)"];
 for (let i = 0; i < websites.length; i++) {
   text += websites[i] + "<br>";
 }
